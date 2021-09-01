f849501440eb6c7122f8b481ac2cc09217b2128f
MaryJaneLouise <mj.louise007@gmail.com>
2021-09-01 19:16:51 +0800

    INITIAL COMMIT

---
+++ Module 1 Lessons/Merging Arrays.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ModuleOneMergArr() {
+    int i, j;
+    int tempArray, numFirstArray, numSecondArray, arrayFirst[128], arraySecond[128], finalArray[128];
+    printf("Enter the number of elements to be entered in the first array: ");
+    scanf("%d", &numFirstArray);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < numFirstArray; i++) {
+        printf("1st Array | Element [%d] = ", i);
+        scanf("%d", &arrayFirst[i]);
+        finalArray[i] = arrayFirst[i];
+    }
+
+    j = i;
+    printf("\nEnter the number of elements to be entered in the second array: ");
+    scanf("%d", &numSecondArray);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < numSecondArray; i++) {
+        printf("2nd Array | Element [%d] = ", i);
+        scanf("%d", &arraySecond[i]);
+        finalArray[j] = arraySecond[i];
+        j++;
+    }
+
+    printf("\nResult of the merging two arrays:\n");
+    for (i = 0; i < j; i++) {
+        printf("Element [%d] = %d\n", i, finalArray[i]);
+    }
+}
+
+int main() {
+    ModuleOneMergArr();
+    return 0;
+}
+
+

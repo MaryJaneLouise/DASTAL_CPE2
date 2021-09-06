897c99617f1028bb4dc468f16c7038ea34d50e12
MaryJaneLouise <mj.louise007@gmail.com>
2021-09-06 20:38:02 +0800

    INITIAL COMMIT

--- Module 2/Activity 1.c
+++ Module 2/Activity 1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 void ModuleTwoActivityOne() {
+    int i;
     int arrayEx[7] = {1, 2, 3, 4, 5, 6, 7};
     int *pointerArray;
-    int i;
 
     /*printf("The elements found at the original array is:");
     for (i = 0; i < 7; i++) {

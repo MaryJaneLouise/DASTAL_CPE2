aacd0ebb47c4a0ad623d374ea84d7b23ee30041e
MaryJaneLouise <mj.louise007@gmail.com>
2021-09-13 18:18:17 +0800

    INITIAL COMMIT

--- Module 2/Module 2 Lessons/Two-Dimensional Arrays.c
+++ Module 2/Module 2 Lessons/Two-Dimensional Arrays.c
@@ -127,10 +127,51 @@ void TwoDimensionalArraysEx3() {
 
 }
 
+void TwoDimensionalArraysEx4() {
+
+    int i, j;
+    int rowPascal, pascalArray[128][128];
+
+    printf("Enter the number of rows to be printed in the Pascal's Triangle: ");
+    scanf("%d", &rowPascal);
+
+    if (rowPascal > 0) {
+        printf("\nResult:\n");
+        for (i = 0; i < rowPascal; i++) {
+            for (j = 0; j <= i; j++) {
+                if (i == 0 || j == 0) {
+                    pascalArray[i][j] = 1;
+                }
+                else {
+                    pascalArray[i][j] = pascalArray[i - 1][j - 1] + pascalArray[i - 1][j];
+                }
+
+                printf("%d", pascalArray[i][j]);
+                if (i - j == 0) {
+                    printf("");
+                }
+                else {
+                    printf("\t");
+                }
+            }
+            printf("\n");
+        }
+    }
+
+    else if (rowPascal == 0) {
+        printf("Unfortunately, the program cannot print empty rows. Try again.");
+    }
+
+    else {
+        printf("Unfortunately, the program cannot print negative rows. Try again.");
+    }
+}
+
 int main() {
     //TwoDimensionalArraysEx1();
     //TwoDimensionalArraysEx2();
     //TwoDimensionalArraysEx3();
+    TwoDimensionalArraysEx4();
     return 0;
 
 }

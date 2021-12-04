dc9f290ee5eac2f555bc92d4350c3e4094da0a9d
MaryJaneLouise <mj.louise007@gmail.com>
2021-12-04 21:37:05 +0800

    INITIAL COMMIT

--- Module 6/Activity 1.c
+++ Module 6/Activity 1.c
@@ -1,73 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
-    int vertex;
-    struct node* next;
-};
-struct node* createNode(int);
+#define numVertices 5
 
-struct Graph {
-    int numVertices;
-    struct node** adjLists;
+struct storeGraph {
+    struct Node* head[numVertices];
 };
 
-// Create a node
-struct node* createNode(int v) {
-    struct node* newNode = malloc(sizeof(struct node));
-    newNode->vertex = v;
-    newNode->next = NULL;
-    return newNode;
-}
+struct Node {
+    int destination;
+    struct Node* next;
+};
 
-// Create a graph
-struct Graph* createAGraph(int vertices) {
-    struct Graph* graph = malloc(sizeof(struct Graph));
-    graph->numVertices = vertices;
+struct edgeGraph {
+    int source, destination;
+};
 
-    graph->adjLists = malloc(vertices * sizeof(struct node*));
+struct storeGraph* createGraph(struct edgeGraph edges[], int n) {
+    struct storeGraph* graph = (struct storeGraph*)malloc(sizeof(struct storeGraph));
 
-    int i;
-    for (i = 0; i < vertices; i++)
-        graph->adjLists[i] = NULL;
+    for (int i = 0; i < numVertices; i++) {
+        graph->head[i] = NULL;
+    }
 
+    for (int i = 0; i < n; i++) {
+        int source = edges[i].source;
+        int destination = edges[i].destination;
+
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->destination = destination;
+        newNode->next = graph->head[source];
+        graph->head[source] = newNode;
+    }
     return graph;
 }
 
-// Add edge
-void addEdge(struct Graph* graph, int s, int d) {
-    // Add edge from s to d
-    struct node* newNode = createNode(d);
-    newNode->next = graph->adjLists[s];
-    graph->adjLists[s] = newNode;
-
-    // Add edge from d to s
-    newNode = createNode(s);
-    newNode->next = graph->adjLists[d];
-    graph->adjLists[d] = newNode;
-}
-
-// Print the graph
-void printGraph(struct Graph* graph) {
-    int v;
-    for (v = 0; v < graph->numVertices; v++) {
-        struct node* temp = graph->adjLists[v];
-        printf("\n Vertex %d\n: ", v);
-        while (temp) {
-            printf("%d -> ", temp->vertex);
-            temp = temp->next;
+void printGraph(struct storeGraph* graph) {
+    for (int i = 0; i < numVertices; i++) {
+        struct Node* pointerNode = graph->head[i];
+        while (pointerNode != NULL) {
+            printf("[Point %d —> Point %d]\t", i, pointerNode->destination);
+            pointerNode = pointerNode->next;
         }
         printf("\n");
     }
 }
 
 int main() {
-    struct Graph* graph = createAGraph(4);
-    addEdge(graph, 0, 1);
-    addEdge(graph, 0, 2);
-    addEdge(graph, 0, 3);
-    addEdge(graph, 1, 2);
-
+    struct edgeGraph edges[] = {
+                    {0, 1},
+                    {1, 3},
+                    {2, 1},
+                    {3, 4},
+                    {4, 2}
+    };
+    int n = sizeof(edges)/sizeof(edges[0]);
+    struct storeGraph *graph = createGraph(edges, n);
+    printf("Note: A = 0, B = 1...\n\nDirected Graph:\n");
     printGraph(graph);
 
     return 0;
